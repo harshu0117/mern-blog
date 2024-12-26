a69890a5971748bd9b34f6960904d69689ff34bf
harshu0117 <iamharsha0117@gmail.com>
2024-12-26 23:08:19 +0530

    sidebar dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 export default function Dasjboard() {
+  const loacation = useLocation();
+  const [tab,setTab] = useState('');
+
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  },[loacation.search]);
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen felx felx-col md:flex-row">
+      <div className="md:w-56 ">
+        <DashSidebar/>
+      </div>
+        {tab==='profile' && <DashProfile/>}
     </div>
   )
 }
